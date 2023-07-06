a2dfc08747fc0ab0c54477dade5f4a9a5e5f8f8b
wstan76 <wstan76@gmail.com>
2023-07-07 03:11:19 +0800

    first commit

---
+++ scripts/deploy.py
@@ -0,0 +1,25 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (get_account, deploy_mock, LOCAL_BLOCKCHAIN_ENVIRONMENTS)
+
+def deploy_fund_me():
+    account = get_account()
+    
+    # If we are on a persistent network like Sepolia, use the associated address
+    # otherwise, deploy mocks
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
+    else:
+        deploy_mock()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()]["verify"]
+    )
+    print(f"Contract deployed to {fund_me.address}")
+    return fund_me
+
+def main():
+    deploy_fund_me()
+    
